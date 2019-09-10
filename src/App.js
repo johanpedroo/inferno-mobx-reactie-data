c21c4d2b7d1a2aca6807c9145dd9697dbffef93f
Johan Pedro Inês <johanpedroo@gmail.com>
2019-09-10 01:03:27 -0300

    multi level components with mobx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { version, Component } from 'inferno';
+import {Component, version} from 'inferno';
 import Logo from './logo';
 import './App.css';
+import {observable} from "mobx";
+import {observer} from 'inferno-mobx';
 
-
-class App extends Component {
+@observer
+class App {
   constructor(props) {
-    super(props);
     Object.assign(this, props);
-    console.log(this)
   }
 
   render() {
@@ -16,7 +16,7 @@ class App extends Component {
         <header className="App-header">
           <Logo width="80" height="80" />
           <p>{`Welcome to Inferno ${version}`}</p>
-          <button onClick={() => {this.teste = '12345'; console.log('clicked')}}>
+          <button onClick={() => {this.teste = '12345'; console.log(this)}}>
             {this.teste}
           </button>
         </header>
@@ -28,22 +28,26 @@ class App extends Component {
 
 
 class Teste extends App {
-  teste = 'dsdsd';
-  teste2;
-  x;
-  y;
-}
+  @observable teste = 'dsdsd';
+  @observable teste2;
+  @observable x;
+  @observable y;
 
-class Window extends Teste {
   constructor(props) {
     super(props);
-    Object.assign(this, props);
+    this.teste = props.teste || this.teste;
   }
 
+}
+
+
+class Window extends Teste {
+  @observable content = [];
   render() {
     return <div className={'app'}>
+      {super.render()}
       {
-        this.content.map((Child,i) => <div key={i}><Child/></div>)
+        this.content.map((Child,i) => <div key={i}>{Child.render()}</div>)
       }
     </div>
   }
@@ -55,15 +59,20 @@ function constructWindow(props){
   return window.aWindow = new Window(props)
 }
 
-const TesteWindow = constructTeste({aaa: 'dsdsds', f(){
-  console.log(Teste.teste = 'clicked')
-    Teste.forceUpdate()
-}});
+const TesteWindow = constructTeste({aaa: 'dsdsds'});
 
 const AWindow = constructWindow({
   content: [
       TesteWindow
-  ]
+  ],
+  teste: '1111'
 });
 
-export default AWindow;
+@observer
+class CreateWindow extends Component {
+  render(){
+    return this.props.data.render()
+  }
+}
+
+export default () => <CreateWindow data={AWindow}/>;
